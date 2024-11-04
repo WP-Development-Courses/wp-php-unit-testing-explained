1376452f143f659cbe6e8ad0e981f4f05d36f32c
Fränk Klein <frank@kleinconsulting.lu>
2024-11-04 10:43:03 +0100

    Completed first unit test.

--- byline-v1/tests/test-get-byline.php
+++ byline-v1/tests/test-get-byline.php
@@ -1,6 +1,9 @@
 <?php
 class Test_Get_Byline extends WP_UnitTestCase {
-	public function test_sample() {
-		$this->assertTrue( true );
+	public function test_is_byline_returned() {
+		$this->assertSame(
+			'Written by Max Mustermann',
+			wput_get_byline( 'Max Mustermann' )
+		);
 	}
 }
