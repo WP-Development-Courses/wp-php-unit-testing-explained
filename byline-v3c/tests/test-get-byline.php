13d9b3ba5671929d9e934dc99a99bbb418a3112f
Fränk Klein <frank@kleinconsulting.lu>
2024-11-10 15:34:11 +0100

    Refacot tests to use the User unit test factory.

---
+++ byline-v3c/tests/test-get-byline.php
@@ -0,0 +1,20 @@
+<?php
+class Test_Get_Byline extends WP_UnitTestCase {
+	public function test_is_byline_returned() {
+		$this->assertSame(
+			'Written by Max Mustermann',
+			wput_get_byline( 'Max Mustermann' )
+		);
+	}
+
+	public function test_is_byline_returned_for_user() {
+		$author = self::factory()->user->create_and_get( [
+			'display_name' => 'John Doe',
+		] );
+
+		$this->assertSame(
+			'Written by John Doe',
+			wput_get_byline_for_user( $author )
+		);
+	}
+}
