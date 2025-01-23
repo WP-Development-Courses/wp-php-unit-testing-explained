73349f54155c0f68510a31c16a7c5c5977820549
Fränk Klein <frank@kleinconsulting.lu>
2025-01-23 16:16:12 +0100

    Understanding Setup and Teardown: Add code samples.

---
+++ code-samples/test-sample.php
@@ -0,0 +1,14 @@
+<?php
+class Test_Sample extends WP_UnitTestCase {
+	public function test_1() {
+		// Create fixtures
+		// Run assertions
+		// Delete fixtures
+	}
+
+	public function test_2() {
+		// Create fixtures
+		// Run assertions
+		// Delete fixtures
+	}
+}
---
+++ code-samples/wpunit-sample-setup-teardown.php
@@ -0,0 +1,14 @@
+<?php
+class WP_Test extends WP_UnitTestCase {
+	public static function wpSetUpBeforeClass() {}
+
+	public static function wpTearDownAfterClass() {}
+
+	public function set_up() {
+		parent::set_up();
+	}
+
+	public function tear_down() {
+		parent::tear_down();
+	}
+}
